87f7bfe19344833228c344e4f60d79511b3a1fc7
ZacharyBunch <zachbunch2002@gmail.com>
2026-02-11 14:13:53 -0500

    organizing

--- Metabarcoding.R
+++ Metabarcoding.R
@@ -1,4 +1,11 @@
+####Libraries ####
+
 library(readxl)
+library(ggplot2)
+library(maps)
+
+
+#### File Read ####
 
 Sample_data <- read.csv("samples.csv", skip = 1)
 
@@ -6,7 +13,7 @@ BK_Con <- read.csv("2025_BK_Con.csv")
 
 BK_Con_first4 <- substr(colnames(BK_Con)[-1], 1, 4)
 
-
+#### Filtering ####
 
 
 Filtered_samples <- Sample_data[Sample_data$Shortid %in% BK_Con_first4, ]
@@ -17,3 +24,29 @@ Filtered_samples <- Filtered_samples[
 
 
 write.csv(Filtered_samples, "Filtered_samples.csv")
+
+
+### Mapping ####
+
+ggplot(Filtered_samples, aes(x = Collectionsite.Long, y = Collectionsite.Lat)) +
+  borders("state") +
+  geom_point(alpha = 0.7) +
+  coord_quickmap() +
+  theme_minimal() +
+  labs(x = "Longitude", y = "Latitude", title = "Sample Locations")
+
+
+
+us_map <- map_data("state")
+
+ggplot() +
+  geom_polygon(data = us_map,
+               aes(x = long, y = lat, group = group),
+               fill = "gray90", color = "white") +
+  geom_point(data = Filtered_samples,
+             aes(x = Collectionsite.Long, y = Collectionsite.Lat),
+             color = "red", size = 2, alpha = 0.7) +
+  coord_quickmap(xlim = c(-125, -66), ylim = c(24, 50)) +
+  theme_minimal() +
+  labs(x = "Longitude", y = "Latitude",
+       title = "Sample Locations Across the United States")
